25795a7fb3cf3b024c9e29a92835284cfda34086
Obec <116595408+PrinceOlubari@users.noreply.github.com>
2023-10-19 23:57:26 +0100

    using setTimeout to handle refresh

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerText = currentYear + 1;
 
 function updateCountDown() {
   const currentTime = new Date();
@@ -16,9 +20,16 @@ function updateCountDown() {
   const s = Math.floor(diff / 1000) % 60;
 
   days.innerHTML = d;
-  hours.innerHTML = h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
 }
+
+//show spinner before countdown
+
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
 
 setInterval(updateCountDown, 1000);
